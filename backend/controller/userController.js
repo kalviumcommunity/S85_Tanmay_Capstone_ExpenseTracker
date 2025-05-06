49857665abd96cf1b34f8ec5ba5d4042c7d56611
tanmay-1211 <tanmayskbhatt@gmail.com>
2025-05-06 22:44:52 +0530

    Delete

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -48,5 +48,19 @@ const updateUser = async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 };
+const deleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
 
-module.exports = { createUser,getAllUsers ,updateUser};
+        const deletedUser = await User.findByIdAndDelete(userId);
+
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+module.exports = { createUser,getAllUsers ,updateUser,deleteUser};
